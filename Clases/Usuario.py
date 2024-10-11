d72f61c07ca1078828fd0917c098c9345e389e55
Dnaet <54982189+Dnaet@users.noreply.github.com>
2024-10-11 09:22:58 -0300

    1

--- Clases/Usuario.py
+++ Clases/Usuario.py
@@ -1,5 +1,7 @@
 import re
 from tipo_usuario import Tipo_Usuario
+from rut_chile import rut_chile
+
 
 class Usuario:
     def __init__(self, id_usuario, nombre_usuario, correo_usuario, telefono_usuario, rut_usuario, id_tipo_usuario, habilitado, fecha_creacion):
@@ -13,8 +15,7 @@ class Usuario:
         self.fecha_creacion = fecha_creacion
 
     def validar_rut(self):
-        # Lógica para validar el RUT chileno
-        return True  # Aquí deberías implementar la validación real
+        return rut_chile.is_valid_rut(self.rut_usuario)
 
     def validar_correo(self):
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
