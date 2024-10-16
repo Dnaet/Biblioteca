80d143873a0ef1cff6e9cf80557aa6dad926b77c
Dnaet <54982189+Dnaet@users.noreply.github.com>
2024-10-16 18:21:11 -0300

    DB

--- Biblioteca/Clases/Biblioteca.py
+++ Biblioteca/Clases/Biblioteca.py
@@ -16,3 +16,26 @@ class Biblioteca:
 
     def info(self):
         return f"Biblioteca: {self.nombre_biblioteca}, Dirección: {self.direccion_biblioteca}, Teléfono: {self.telefono_biblioteca}"
+
+
+import mysql.connector
+
+# Connect to server
+cnx = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="mike",
+    password="s3cre3t!")
+
+# Get a cursor
+cur = cnx.cursor()
+
+# Execute a query
+cur.execute("SELECT CURDATE()")
+
+# Fetch one result
+row = cur.fetchone()
+print("Current date is: {0}".format(row[0]))
+
+# Close connection
+cnx.close()
---
+++ DB.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+# Connect to server
+cnx = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="",
+    database="biblioteca")
+
+# Get a cursor
+cur = cnx.cursor()
+
+# Execute a query
+cur.execute("SELECT * from autor()")
+
+# Fetch one result
+row = cur.fetchone()
+print("Current date is: {0}".format(row[0]))
+
+# Close connection
+cnx.close()
